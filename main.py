1ba9470cb6ae4dccf34b4fead340d01de7e6cda2
thankstoalive <thankstoalive@naver.com>
2025-04-18 19:27:14 +0900

    Add POST /todos endpoint

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ class Todo(BaseModel):
     id: int
     title: str
     completed: bool = False
+    
+class TodoCreate(BaseModel):
+    title: str
 
 todos: List[Todo] = []
 
@@ -18,3 +21,10 @@ async def root():
 @app.get("/todos", response_model=List[Todo])
 async def get_todos():
     return todos
+    
+@app.post("/todos", response_model=Todo)
+async def create_todo(todo: TodoCreate):
+    new_id = todos[-1].id + 1 if todos else 1
+    new_todo = Todo(id=new_id, title=todo.title)
+    todos.append(new_todo)
+    return new_todo
